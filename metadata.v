08ba41004a7281c000619156e20de99e2e0883af
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-03-07 17:21:54 +0530

    mention README

--- metadata.v
+++ metadata.v
@@ -20,6 +20,6 @@ information. It retrieves information about ALL available repositories
 belonging to the authenticated user. Both public and private.
 
 Please follow safety precautions when storing access tokens, and read
-the instructions carefully.
+the instructions in README carefully.
 '
 )
